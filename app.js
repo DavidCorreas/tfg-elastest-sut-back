0eff4b14c1f01422cddc37703f3d322e6cfe5c24
DavidCorreas <davidestu77@gmail.com>
2020-05-24 17:19:05 +0200

    38 Fetching initial posts (New enpoint)

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('This is middleware', req.originalUrl);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!!');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {id: "id-dafasdf", title: "First server-side post", content: "This is coming from the server"},
+        {id: "id-adsferw", title: "Second server-side post", content: "This is coming from the server!"}
+    ]
+    res.status(200).json({
+        message: "Post fetched succesfully",
+        posts: posts
+    })
 });
 
 module.exports = app;
