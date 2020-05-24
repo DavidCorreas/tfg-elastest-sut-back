8af6dc1354bac372dff133a5c94ee5d9afa78fec
DavidCorreas <davidestu77@gmail.com>
2020-05-24 17:44:25 +0200

    40 Understanding CORS

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Acces-Control-Methods', 'GET, POST, DELETE, OPTIONS');
+    next();
+})
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         {id: "id-dafasdf", title: "First server-side post", content: "This is coming from the server"},
@@ -10,7 +17,7 @@ app.use('/api/posts', (req, res, next) => {
     res.status(200).json({
         message: "Post fetched succesfully",
         posts: posts
-    })
+    });
 });
 
 module.exports = app;
