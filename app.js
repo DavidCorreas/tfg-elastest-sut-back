217ab6ef0f827cc6b68a794e53579f07c875f94a
DavidCorreas <davidestu77@gmail.com>
2020-05-25 16:03:47 +0200

    55 Fetching data from database

--- app.js
+++ app.js
@@ -36,14 +36,13 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-    const posts = [
-        {id: "id-dafasdf", title: "First server-side post", content: "This is coming from the server"},
-        {id: "id-adsferw", title: "Second server-side post", content: "This is coming from the server!"}
-    ]
-    res.status(200).json({
-        message: "Post fetched succesfully",
-        posts: posts
-    });
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: "Post fetched succesfully",
+                posts: documents
+            });
+        });
 });
 
 module.exports = app;
