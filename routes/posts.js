4a315db7158a7b84c257ac39147c5ff10e3a57ac
DavidCorreas <davidestu77@gmail.com>
2020-05-28 19:47:12 +0200

    104 Adding Middleware to protect routes

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 
 const router = express.Router();
 const Post = require("../models/post");
+const checkAuth = require("../middleware/check-auth");
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -29,6 +30,7 @@ const storage = multer.diskStorage({
 // Anadir posts a la BD
 router.post(
   "",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
@@ -56,25 +58,26 @@ router.get("", (req, res, next) => {
   const postQuery = Post.find();
   let fetchedPosts;
   if (pageSize && currentPage) {
-    postQuery
-      .skip(pageSize * (currentPage - 1))
-      .limit(pageSize);
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
   }
-  postQuery.then((documents) => {
-    fetchedPosts = documents;
-    return Post.countDocuments();
-  }).then(count => {
-    res.status(200).json({
-      message: "Post fetched succesfully",
-      posts: fetchedPosts,
-      maxPosts: count
+  postQuery
+    .then((documents) => {
+      fetchedPosts = documents;
+      return Post.countDocuments();
+    })
+    .then((count) => {
+      res.status(200).json({
+        message: "Post fetched succesfully",
+        posts: fetchedPosts,
+        maxPosts: count,
+      });
     });
-  });
 });
 
 // Actualizar post
 router.put(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -107,7 +110,7 @@ router.get("/:id", (req, res, next) => {
 });
 
 // Borrar Post de la BD
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   console.log("Deleting post: " + req.params.id);
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     console.log(result);
