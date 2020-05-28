69e39e42943d9848fb948fc202e9c5a6c976c19e
DavidCorreas <davidestu77@gmail.com>
2020-05-28 13:49:19 +0200

    99 Creating a new user upon request

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
-const postsRoutes = require('./routes/posts');
-const userRoutes = require('./routes/user');
+const mongoose = require("mongoose");
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 const app = express();
 
 // Conectar a mongo DB
-mongoose.connect('mongodb+srv://david:D8UNgvlyVFskeMZ8@cluster0-nowfg.mongodb.net/node-angular?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => {
-        console.log('Connected to database');
-    })
-    .catch(() => {
-        console.log('Connection failed!');
-    });
+mongoose
+  .connect(
+    "mongodb+srv://david:D8UNgvlyVFskeMZ8@cluster0-nowfg.mongodb.net/node-angular?retryWrites=true&w=majority",
+    { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
-// Usar parsers para coger informacion de los bodys y url de los mensajes http 
+// Usar parsers para coger informacion de los bodys y url de los mensajes http
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join('images')))
+app.use("/images", express.static(path.join("images")));
 
 // Anadir cabeceras para CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+  );
+  next();
 });
 
-app.use('/api/posts', postsRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 // Exportar app al server
 module.exports = app;
