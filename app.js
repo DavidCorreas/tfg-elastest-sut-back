71b01a18219410cdf9a7865da3a51d81c1f9370c
DavidCorreas <davidestu77@gmail.com>
2020-05-28 13:25:35 +0200

    98 Creating user model

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
-const postsRoutes = require('./routes/posts')
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 const app = express();
 
 // Conectar a mongo DB
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
 
 // Exportar app al server
 module.exports = app;
