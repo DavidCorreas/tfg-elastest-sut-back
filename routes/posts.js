033f65788a04978c78cc9c1f78d1148667b45601
DavidCorreas <davidestu77@gmail.com>
2020-05-29 18:09:07 +0200

    130 Using Controllers

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const multer = require("multer");
-
+const postController = require('../controllers/post');
 const router = express.Router();
-const Post = require("../models/post");
 const checkAuth = require("../middleware/check-auth");
 
 const MIME_TYPE_MAP = {
@@ -32,114 +31,24 @@ router.post(
   "",
   checkAuth,
   multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename,
-      creator: req.userData.userId
-    });
-    post.save().then((createdPost) => {
-      res.status(201).json({
-        post: {
-          ...createdPost,
-          id: createdPost._id,
-        },
-        message: "Post added successfully!",
-      });
-    }).catch(() => {
-      res.status(500).json({
-        message: 'Creating a post failed!'
-      })
-    });
-  }
+  postController.addPost
 );
 
 // Coger todos los posts de la BD
-router.get("", (req, res, next) => {
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (pageSize && currentPage) {
-    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
-  }
-  postQuery
-    .then((documents) => {
-      fetchedPosts = documents;
-      return Post.countDocuments();
-    })
-    .then((count) => {
-      res.status(200).json({
-        message: "Post fetched succesfully",
-        posts: fetchedPosts,
-        maxPosts: count,
-      });
-    })
-    .catch(() => {
-      res.status(500).json({ message: 'Fetching post falied!' })
-    });
-});
+router.get("", postController.getAllPosts);
 
 // Actualizar post
 router.put(
   "/:id",
   checkAuth,
   multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-      const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename;
-    }
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath,
-      creator: req.userData.userId
-    });
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-      console.log(result);
-      if (result.n > 0) {
-        res.status(200).json({ message: "Update successful!" });
-      } else {
-        res.status(401).json({ message: 'Not authorized!' })
-      }
-    }).catch(() => {
-      res.status(500).json({ message: 'Couldn\'t update post' })
-    });
-  }
+  postController.updatePost
 );
 
 // Obtener post por id
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found!" });
-    }
-  })
-  .catch(() => {
-    res.status(500).json({ message: 'Fetching post falied!' })
-  });
-});
+router.get("/:id", postController.getPost);
 
 // Borrar Post de la BD
-router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
-    console.log(result);
-    if (result.n > 0) {
-      res.status(200).json({ message: "Post deleted!" });
-    } else {
-      res.status(401).json({ message: 'Not authorized!' })
-    }
-  })
-  .catch(() => {
-    res.status(500).json({ message: 'Deleting post falied!' })
-  });
-});
+router.delete("/:id", checkAuth, postController.deletePost);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,60 +1,8 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-
+const UserController = require('../controllers/user');
 const router = express.Router();
 
-router.post("/singup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-    });
-    user
-      .save()
-      .then((result) => {
-        res.status(200).json({
-          message: "User created!",
-          result: result,
-        });
-      })
-      .catch(() => {
-        res.status(500).json({
-          message: 'Failed when creating user',
-        });
-      });
-  });
-});
-
-router.post("/login", (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        throw new Error();
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then((result) => {
-      if (!result) {
-        throw new Error();
-      }
-      const token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        "secret_this_should_be_longer",
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600, 
-        userId: fetchedUser._id
-      });
-    })
-    .catch(() => {
-      return res.status(401).json({ message: "Invalid authentication credentials" });
-    });
-});
+router.post("/singup", UserController.createUser);
+router.post("/login", UserController.userLogin);
 
 module.exports = router;
